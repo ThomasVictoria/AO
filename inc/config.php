45e3675cc473a945353247d562f2d48558a44f58
Thomas Victoria <thomas.victoria@hetic.net>
2016-02-08 13:38:47 +0100

    debug sms

--- inc/config.php
+++ inc/config.php
@@ -1,9 +1,9 @@
 <?php
 
 // Identifiants à la BDD
-define('DB_HOST','');
-define('DB_NAME','');
-define('DB_USER','');
+define('DB_HOST','localhost');
+define('DB_NAME','callr');
+define('DB_USER','root');
 define('DB_PASS','');
 
 define("SALT","9hb876s06xclkg461i6h4y1n1dskfpcm6ej2h30cxjxhrqcp8f");
@@ -70,9 +70,9 @@ $admins = array(
 
 $config = array(
 
-  'journal'  => '',
-  'numero'   => '',
-  'compteur' => ''
+  'journal'  => 'messages',
+  'numero'   => 'test',
+  'compteur' => 'compteur'
 
 );
 
