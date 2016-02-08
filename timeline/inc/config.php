06fa0034cceadccd4c2a6f50d0bf0e380f8e8232
tjabbe <martin.goutry@hetic.net>
2016-02-08 13:15:19 +0100

    Add timeline

---
+++ timeline/inc/config.php
@@ -0,0 +1,93 @@
+<?php
+
+// Identifiants à la BDD
+define('DB_HOST','');
+define('DB_NAME','');
+define('DB_USER','');
+define('DB_PASS','');
+
+define("SALT","9hb876s06xclkg461i6h4y1n1dskfpcm6ej2h30cxjxhrqcp8f");
+
+define("PWDPAULINE","5eef8a2d9d90ceb22949ec268807d2a9d9ba7c59");
+define("PWDMARGAUX","5eef8a2d9d90ceb22949ec268807d2a9d9ba7c59");
+
+
+// Chemins vers les différents chemins utiles au fonctionnement du site
+
+define('CONTROLEURS', 'pages/');
+
+error_reporting(E_ALL);
+ini_set('display_errors',1);
+
+/** 
+ * 
+ * STRUCTURE BASES : 
+ * 
+   TYPE TABLE MESSAGES (nom a spécifié dans le tableaux config) : 
+      - id        INT PRIMARY AUTO_INCREMENT
+      - message   TEXT 250
+      - time      INT
+      - name      VARCHAR
+ *
+ *
+ *
+   TYPE TABLE NUMERO (nom a spécifié dans le tableaux config) : 
+      - id        INT PRIMARY AUTO_INCREMENT
+      - number    INT
+ *    
+ *    
+   TYPE TABLE COMPTEUR (nom a spécifié dans le tableaux config) :
+      - id        INT PRIMARY AUTO_INCREMENT
+      - num       INT
+      -> insert dans cette table un row avec 0 dans num pour que ça fonctionne
+      
+ */
+
+/** 
+ * 
+ * Pour ajouter un admin ajoutez le numéro en premier et le nom 
+ * son nom en second
+ * 
+ */
+
+$admins = array(
+
+  //[numero, nom]
+  ['', 'Pauline'],
+  ['', 'Margaux']  
+
+);
+
+/** 
+  * 
+  * var journal = nom de TABLE MESSAGES
+  *
+  * var numero  = nom de TABLE NUMERO
+  *
+  * var count   = nom de TABLE 
+  * 
+  */
+
+$config = array(
+
+  'journal'  => '',
+  'numero'   => '',
+  'compteur' => ''
+
+);
+
+try
+{
+  // Try to connect to database
+  $pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME,DB_USER,DB_PASS);
+
+  // Set fetch mode to object
+  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+}
+catch (Exception $e)
+{
+  // Failed to connect
+  die('Could not connect');
+}
+
+?>
---
+++ timeline/index.php
@@ -0,0 +1,54 @@
+<?php include 'header.php' ?>
+
+<div class="row animated fadeInRight">
+  <div class="col-lg-12">
+    <div class="ibox float-e-margins">
+      <div class="text-center float-e-margins p-md">
+        <img src="img/logo_MP.png" class="logo-timeline" alt="">
+        <h1>Timeline 4L Trophy MP Gazelles</h1>
+      </div>
+      <div class="ibox-content" id="ibox-content">
+
+        <div id="vertical-timeline" class="vertical-container dark-timeline center-orientation">
+
+          <?php 
+
+
+          $ch = curl_init('http://91.121.107.122/api/');
+          curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+          $response = curl_exec($ch);
+          $response = json_decode($response);
+
+          if ($response->code == 200) {
+
+          foreach($response->messages as $message){
+
+            $date = date('d/m-G:i', $message->time);
+            $date = explode("-", $date);
+
+          ?>
+
+          <div class="vertical-timeline-block">
+            <div class="vertical-timeline-icon navy-bg">
+              <i class="fa fa-comment"></i>
+            </div>
+            <div class="vertical-timeline-content">
+              <h2><?php echo $message->name ?></h2>
+              <p><?php echo $message->message ?></p>
+              <span class="vertical-date">
+                <?php echo $date[0] ?> <br/>
+                <small><?php echo $date[1] ?></small>
+              </span>
+            </div>
+          </div>
+
+          <?php }} ?>
+
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+<?php include 'footer.php' ?>
