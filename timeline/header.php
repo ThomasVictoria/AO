f7435303ec5859044a3db76645dcf36fd174300e
tjabbe <martin.goutry@hetic.net>
2016-02-09 21:52:56 +0100

    change wording

--- timeline/header.php
+++ timeline/header.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>MP GAZ'L Board</title>
+    <title>MP GAZ'L - 4L Trophy</title>
 
     <link rel="shortcut icon" href="favicon.ico">
     <link href="css/bootstrap.min.css" rel="stylesheet">
