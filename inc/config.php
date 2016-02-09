d35df505c721198038f7830cfa3bb599895690d8
tjabbe <martin.goutry@hetic.net>
2016-02-09 19:06:27 +0100

    fix

--- inc/config.php
+++ inc/config.php
@@ -78,18 +78,18 @@ $config = array(
 
 );
 
-// try
-// {
-//   // Try to connect to database
-//   $pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME,DB_USER,DB_PASS);
+try
+{
+  // Try to connect to database
+  $pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME,DB_USER,DB_PASS);
 
-//   // Set fetch mode to object
-//   $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
-// }
-// catch (Exception $e)
-// {
-//   // Failed to connect
-//   die('Could not connect');
-// }
+  // Set fetch mode to object
+  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+}
+catch (Exception $e)
+{
+  // Failed to connect
+  die('Could not connect');
+}
 
 ?>
